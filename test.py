1fa78ecab9e80091f61573a282f9bbd89fe2788e
Satyam Purohit <78374299+Sats2812@users.noreply.github.com>
2021-05-05 15:45:54 +0530

    Update test.py

--- test.py
+++ test.py
@@ -8,11 +8,11 @@ driver.get("https://opensea.io/assets/ens?search[categories][0]=domain-names&sea
 new=[]
 current=[]
 last=[]
-
-for i in range(0,5):                #Change the range according to the data required by you for the number of scrolls
-
-    driver.execute_script("window.scrollTo(0,document.body.scrollHeight)","return document.documentElement.outerHTML")
-    sleep(6)
+sleep(3)
+previous_height=driver.execute_script("return document.body.scrollHeight","return document.documentElement.outerHTML")
+while True:
+    driver.execute_script('window.scrollTo(0,document.body.scrollHeight)',"return document.documentElement.outerHTML")
+    sleep(3)
     soup=BeautifulSoup(driver.page_source,'lxml')
     box=soup.find('div',{'class':'Blockreact__Block-sc-18q9hu0-0 hxevxe AssetsSearchView--assets'})
     all_hackathons =box.find_all('div',{'class': 'Blockreact__Block-sc-18q9hu0-0 hxevxe'})
@@ -23,6 +23,20 @@ for i in range(0,5):                #Change the range according to the data requ
         current.append(name)       
         date=hackathon.find('div',{'class':'Pricereact__DivContainer-t54vn5-0 jsOkdo Price--main'}).text[1:]
         last.append(date)
+
+        print(h_type)  # Name of the Etherium
+
+        print(name)   # Price of the Eth
+        
+        print(date) #Last price
+    
+    new_height=driver.execute_script("return document.body.scrollHeight","return document.documentElement.outerHTML")
+    if new_height==previous_height:
+        break
+    previous_height=new_height
+
+    
+
 df = pd.DataFrame(list(zip(new,current,last)),
 columns =['Name', 'current','Last'])
 df.to_csv("Etherium.csv")
